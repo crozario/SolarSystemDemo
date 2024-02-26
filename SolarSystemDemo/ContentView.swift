5a2e48dbd5514ef533fa2626ff2aca60c730a4fe
Crossley Rozario <crozariodev@gmail.com>
2024-02-26 14:42:36 -0500

    Initial Commit

---
+++ SolarSystemDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SolarSystemDemo
+//
+//  Created by Crossley Rozario on 2/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SolarSystemDemo/SolarSystemDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemDemoApp.swift
+//  SolarSystemDemo
+//
+//  Created by Crossley Rozario on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
